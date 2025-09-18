4ce319eb4ac5a19b2ecd1c1b8c7a6d417f6ef772
Mahad Jafri <mahadjafri@gmail.com>
2025-09-19 04:22:31 +0500

    Interactive date counter with adjustable step size

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [Count, setCount] = useState(0);
+  const dateToday = new Date();
+  dateToday.setDate(dateToday.getDate() + Count);
+  console.log(dateToday);
+  console.log(Math.abs(Count));
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span> Step: {step} </span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span> Count: {Count} </span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      {Count === 0 && <p>Today is {dateToday.toDateString()}</p>}
+      {Count > 0 && (
+        <p>
+          {Math.abs(Count)} days from today is {dateToday.toDateString()}
+        </p>
+      )}
+      {Count < 0 && (
+        <p>
+          {Math.abs(Count)} days ago was {dateToday.toDateString()}
+        </p>
+      )}
+    </div>
+  );
+}
